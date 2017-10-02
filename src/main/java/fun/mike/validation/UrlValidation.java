e6682a119d39844bf4db3a3e485958bb265c3a3d
EAS7510 <michaeleaster@northwesternmutual.com>
2017-10-01 22:03:53 -0500

    Clean up URL validation bean.

--- src/main/java/fun/mike/validation/UrlValidation.java
+++ src/main/java/fun/mike/validation/UrlValidation.java
@@ -1,40 +1,53 @@
 package fun.mike.validation;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 public class UrlValidation {
-    private final String url;
+    private final String value;
     private final boolean isValid;
-    private final List<String> problems;
+    private final String problem;
+
+    private UrlValidation(String value, boolean isValid, String problem) {
+        this.value = value;
+        this.isValid = isValid;
+        this.problem = problem;
+    }
+
+    public static UrlValidation valid(String value) {
+        return new UrlValidation(value, true, null);
+    }
+
+    public static UrlValidation invalid(String value, String problem) {
+        return new UrlValidation(value, false, problem);
+    }
 
     @Override
     public String toString() {
         return "UrlValidation{" +
-                "url='" + url + '\'' +
+                "value='" + value + '\'' +
                 ", isValid=" + isValid +
-                ", problems=" + problems +
+                ", problem='" + problem + '\'' +
                 '}';
     }
 
-    private UrlValidation(String url, boolean isValid, List<String> problems) {
-        this.url = url;
-        this.isValid = isValid;
-        this.problems = new LinkedList<>(problems);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UrlValidation that = (UrlValidation) o;
+
+        if (isValid != that.isValid) return false;
+        if (value != null ? !value.equals(that.value) : that.value != null) return false;
+        return problem != null ? problem.equals(that.problem) : that.problem == null;
     }
 
-    public static UrlValidation valid(String url) {
-        return new UrlValidation(url, true, new LinkedList<>());
-    }
-
-    public static UrlValidation invalid(String url, List<String> problems) {
-        return new UrlValidation(url, false, problems);
-    }
-
-    public static UrlValidation invalid(String url, String problem) {
-        return new UrlValidation(url, false, Arrays.asList(problem));
+    @Override
+    public int hashCode() {
+        int result = value != null ? value.hashCode() : 0;
+        result = 31 * result + (isValid ? 1 : 0);
+        result = 31 * result + (problem != null ? problem.hashCode() : 0);
+        return result;
     }
 
     public boolean isValid() {
@@ -46,47 +59,19 @@ public class UrlValidation {
             return this;
         }
 
-        String reason = getReason()
-                .orElseThrow(() -> {
-                    String message = String.format("URL \"%s\" is invalid, but reason generation failed.");
-                    return new IllegalStateException(message);
-                });
-        String message = String.format("%s: %s", prefix, reason);
+        String problem = getProblem().orElseThrow(() -> {
+            String message = String.format("\"%s\" is invalid, but no problem found..");
+            return new IllegalStateException(message);
+        });
+        String message = String.format("%s: %s", prefix, problem);
         throw new IllegalArgumentException(message);
     }
 
-    public Optional<String> getReason() {
+    public Optional<String> getProblem() {
         if (isValid) {
             return Optional.empty();
         }
 
-        String reason;
-        if (problems.size() == 1) {
-            reason = problems.get(0);
-        } else {
-            reason = String.format("Multiple problems:\n%s", String.join("\n", problems));
-        }
-
-        return Optional.of(reason);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        UrlValidation that = (UrlValidation) o;
-
-        if (isValid != that.isValid) return false;
-        if (url != null ? !url.equals(that.url) : that.url != null) return false;
-        return problems != null ? problems.equals(that.problems) : that.problems == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = url != null ? url.hashCode() : 0;
-        result = 31 * result + (isValid ? 1 : 0);
-        result = 31 * result + (problems != null ? problems.hashCode() : 0);
-        return result;
+        return Optional.of(problem);
     }
 }
